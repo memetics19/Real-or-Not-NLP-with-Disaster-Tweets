9fc5bb8068c793c4bba34a5703ab094e23079793
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-08-04 01:17:17 +0530

    updated tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -4,9 +4,14 @@ import pandas as pd
 import pytest
 import notebooks.data_cleaning
 
-def test_df_add():
+
+
+
+def test_read_csv():
     new_df =pd.read_csv("./data/train.csv")
     assert new_df is not None
 
-
-
+def test_missing_columns():
+    new_df = pd.read_csv("./data/train.csv")
+    new_df = (new_df.isna().sum()/len(new_df))*100
+    result = new_df.head()
