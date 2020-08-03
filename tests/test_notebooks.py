05c25a855e8fd13a098d2f6ea8a35bbfc734f669
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-08-03 15:01:26 +0530

    updated tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,25 +1,12 @@
 """Test for contradiction of the actual test """
 
 import pandas as pd 
-import unittest
+import pytest
+import notebooks.data_cleaning
+
+def test_df_add():
+    new_df =pd.read_csv("./data/train.csv")
+    assert new_df is not None
 
 
-class DataCleaning(unittest.TestCase):
-    """ test for the contradiction of the data cleaning"""
 
-    def setUp(self):
-        """setup the test model"""
-        data = pd.read_csv('data/train.csv')
-
-
-        return df
-    
-    def drop_missing_values_test(self,data):
-        """" test for the missing values """
-        missing_columns = df.is_null()
-
-        self.assertAlmostEquals(self.data)
-
-
-if __name__ == '__main__':
-    unittest.main()
